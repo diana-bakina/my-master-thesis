f7669ad8692fe5ea7b30d5320920b0ef1715a8d5
Diana Bakina <bakinaaa1@gmail.com>
2025-03-30 21:36:07 +0200

    Minor fix

--- scripts/count.py
+++ scripts/count.py
@@ -6,9 +6,9 @@ repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Path to CSV file
 csv_path = os.path.join(repo_root, "data", "inner_speech.csv")
 
-df = pd.read_csv(csv_path)
+df = pd.read_csv(csv_path, na_values=[], keep_default_na=False)
 
 # Calculate values in the column
-counts = df["inner_speech_type"].value_counts()
+counts = df["inner_speech_function"].value_counts()
 
 print(counts)
